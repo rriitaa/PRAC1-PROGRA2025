7c323615d95333fab87f7e6b9fefcf9c94460abc
Rita <rita.barragan@usp.ceu.es>
2025-02-14 14:08:24 +0100

    reorganizado archivos

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -2,9 +2,11 @@ package aplicacion;
 
 import mates.Matematicas;
 
-public class Principal{
-    public static void main(String[] args){
+public class Principal {
+    public static void main(String[] args) {
         System.out.println("El número PI es " +
                 Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0])));
     }
 }
+// comentario
+// comentario
