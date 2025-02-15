15b5491b51ff8a378d788e558207f435b1ecf029
Rita <rita.barragan@usp.ceu.es>
2025-02-15 13:45:29 +0100

    añadido javadoc

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -3,11 +3,15 @@ package aplicacion;
 import mates.Matematicas;
 
 public class Principal {
+
+    /**
+		 * Inicializa el metodo de matematicas con el parametro que le pasamos
+		 * @param args El numero de pasos que va a dar
+		 */
     public static void main(String[] args) {
         System.out.println("El número PI es " +
                 Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0])));
     }
 }
 // comentario
-// comentario
-//comentario
+
